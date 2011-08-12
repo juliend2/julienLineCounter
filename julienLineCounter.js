3b9dc776b119f1e8fdad5c14d7da946b23754c7f
Julien Desrosiers <juliend2@gmail.com>
2011-08-12 17:42:23 -0400

    [minor] comments

--- julienLineCounter.js
+++ julienLineCounter.js
@@ -10,11 +10,12 @@
 
     // Uh oh! A 'normal' line-height! What's the value in pixels?
     if (lineHeight == 'normal') {
-      var htmlCache = $elm.html();
-      $elm.html('&nbsp;'); // we want its height when it has only ONE line
-      lineHeight = $elm.innerHeight();
-      $elm.html(htmlCache);
+      var htmlCache = $elm.html();      // cache its current content
+      $elm.html('&nbsp;');              // we want its height when it has only ONE line
+      lineHeight = $elm.innerHeight();  // measure it with only one line
+      $elm.html(htmlCache);             // put back its content
     }
+
     var intLineHeight = parseInt(lineHeight, 10);
     nLines = elmHeight / intLineHeight;
 
