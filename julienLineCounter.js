723239edf6f6c0295a608fc8668d4f30b31dd4aa
Julien Desrosiers <juliend2@gmail.com>
2011-08-12 15:26:47 -0400

    initial commit

---
+++ julienLineCounter.js
@@ -0,0 +1,11 @@
+(function( $ ){
+  $.julienLineCounter = {version: 0.1};
+
+  $.julienLineCounter = function( jqSelector ) {
+    var nLines = 0;
+    var $elm = $(jqSelector);
+    if ($elm.length == 0) { return false; }
+    return nLines;
+  };
+
+})( jQuery );
---
+++ spec/JulienLineCounterSpec.js
@@ -0,0 +1,37 @@
+describe("JulienLineCounter", function() {
+
+  describe("when no element is found", function(){
+    it("should return false", function(){
+      expect($.julienLineCounter('#cantbefound')).toEqual(false);
+    });
+  });
+
+  // it("should be able to play a Song", function() {
+  //   player.play(song);
+  //   expect(player.currentlyPlayingSong).toEqual(song);
+
+  //   //demonstrates use of custom matcher
+  //   expect(player).toBePlaying(song);
+  // });
+
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(player.isPlaying).toBeFalsy();
+
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+
+});
